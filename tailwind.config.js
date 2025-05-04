092d4860baf79e0db5cb071f6bb5678fbb58c563
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2025-05-05 00:51:39 +0200

    trying and failing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  content: [
+    "./src/**/*.{ts,tsx,js,jsx,css}",
+    "./components/**/*.{ts,tsx,js,jsx,css}",
+    "./app/**/*.{ts,tsx,js,jsx,css}",
+  ],
+
   theme: {
     extend: {
       colors: {
